b7f00c111e4b58ee66a1e8732327401c93421f17
Vandana Yadav <vandanayadav202004@gmail.com>
2026-04-04 10:28:21 +0530

    Allow all hosts in ALLOWED_HOSTS setting

--- sabkesir/learning_platform/settings.py
+++ sabkesir/learning_platform/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-txmv7u7a75%nng&p9*#w3kg(d)1381mvcvd^0z(giwc#wpe24^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
