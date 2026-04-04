ced1fe9b3068da4b07f0fb78f247e80dd1a9e738
Vandana Yadav <vandanayadav202004@gmail.com>
2026-04-04 10:45:11 +0530

    Fix static and media URL handling in urls.py

--- sabkesir/learning_platform/urls.py
+++ sabkesir/learning_platform/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('', include('app.urls', namespace='live_classes')),
 ]
 
-if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
